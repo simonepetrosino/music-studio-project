4d6dae44b15a8f72f753e724d6807dc7ecdb5e25
simonepetrosino <simone.petrosino@edu.unifi.it>
2025-02-19 17:09:01 +0100

    rimozione codice superuser per motivi di sicurezza

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -8,17 +8,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-import django
-from django.contrib.auth import get_user_model
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-django.setup()
-
-User = get_user_model()
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@example.com", "password123")
-    print("Superuser created successfully!")
 
 application = get_wsgi_application()
